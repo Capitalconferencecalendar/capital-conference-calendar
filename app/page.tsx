8dda51377d6dadcbe9e2aa5af4cdf55b14e1a406
Charlie Nap <linkedin@capitalconferencecalendar.com>
2026-08-18 11:22:09 -0400

    Route homepage to Market View

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
-import EventsPage from "./events/page";
+import { redirect } from "next/navigation";
 
-export default EventsPage;
+export default function HomePage() {
+  redirect("/market-view");
+}
